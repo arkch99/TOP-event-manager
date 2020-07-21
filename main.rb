a7b3a3ba9540b63c672f1510918e329de644fa6d
arkch99 <arko1999@gmail.com>
2020-07-21 22:13:36 +0530

    Method for sanitizing zipcodes

--- main.rb
+++ main.rb
@@ -1,10 +1,21 @@
 require 'csv'
 
-puts "EventManager initialised!"
+def fix_zip(code)
+	if code.nil?
+		code = "00000"
+	elsif code.length > 5
+		code[0..4]
+	else
+		code.rjust(5, "0")
+	end
+end
 
+puts "EventManager initialised!"
 
 contents = CSV.open("event-attendees.csv", headers:true, header_converters: :symbol)
 
 contents.each do |row|
-	puts row[:first_name]
+	name = row[:first_name]
+	zip = fix_zip(row[:zipcode])
+	puts "#{name}: #{zip}"
 end
