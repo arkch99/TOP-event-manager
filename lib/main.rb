95289b2466f2e46367a6203a0741cc3d988f8286
arkch99 <arko1999@gmail.com>
2020-07-23 14:11:35 +0530

    Store generated letters in separate files

--- lib/main.rb
+++ lib/main.rb
@@ -32,6 +32,14 @@ def legislators_from_zip(zip)
   end
 end
 
+def save_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output") # create a directory for letters
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, "w") do |file| #write each letter to a separate file
+    file.puts form_letter
+  end
+end
+
 puts "EventManager initialised!"
 
 contents = CSV.open("event-attendees.csv", headers:true, header_converters: :symbol)
@@ -40,11 +48,13 @@ template_letter = File.read("form_letter.erb")
 erb_template = ERB.new(template_letter)
 
 contents.each do |row|
+  id = row[0] # the index serves as id
   name = row[:first_name]
   zip = fix_zip(row[:zipcode])
   #puts "#{name} #{zip}"  
   legislators = legislators_from_zip(zip)
-  form_letter = erb_template.result(binding)
-  puts form_letter
+  form_letter = erb_template.result(binding) # generate the letter
+  #puts form_letter
   #puts "\n#{name} #{zip} #{legislators}"
+  save_letter(id, form_letter)
 end
